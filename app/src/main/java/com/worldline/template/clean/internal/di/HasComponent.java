ef43cc219889a8beae278581132d2690a752c762
Francisco Javier Martin Gonzalez <javier.marting@atos.net>
2016-09-02 13:50:28 +0200

    Revert "Commit template clean"
    This reverts commit 4389d4989a4ae3e8dbe01ce9af79a44a58b74334.

--- app/src/main/java/com/worldline/template/clean/internal/di/HasComponent.java
+++
@@ -1,8 +0,0 @@
-package com.worldline.template.clean.internal.di;
-
-/**
- * Interface representing a contract for clients that contains a component for dependency injection.
- */
-public interface HasComponent <C> {
-    C getComponent();
-}
