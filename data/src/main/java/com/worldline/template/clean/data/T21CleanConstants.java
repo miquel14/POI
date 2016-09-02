1f757ab5e73845bd21cf1c5cd76b65a6507c4cdb
Francisco Javier Martin Gonzalez <javier.marting@atos.net>
2016-09-02 14:02:09 +0200

    Revert "Revert "Commit template clean""
    This reverts commit ef43cc219889a8beae278581132d2690a752c762.

---
+++ data/src/main/java/com/worldline/template/clean/data/T21CleanConstants.java
@@ -0,0 +1,9 @@
+package com.worldline.template.clean.data;
+
+/**
+ * General constants of the project
+ * TODO: change the name of class/interface
+ */
+public interface T21CleanConstants {
+    // TODO Add all the general constants needed
+}
