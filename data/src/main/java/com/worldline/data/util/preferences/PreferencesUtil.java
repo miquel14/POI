0656d0ec145b1680074aea64bbd2c98c431fc0ec
Marc González Díez <marc.gonzalez@worldline.com>
2017-01-12 09:49:51 +0100

    Add some usefull methods to deal with shared preferences

--- data/src/main/java/com/worldline/data/util/preferences/PreferencesUtil.java
+++ data/src/main/java/com/worldline/data/util/preferences/PreferencesUtil.java
@@ -13,6 +13,10 @@ import javax.inject.Singleton;
 @Singleton
 public class PreferencesUtil {
 
+    private static final String STRING_DEF_VALUE = "";
+
+    private static final int INT_DEF_VALUE = 0;
+
     private SharedPreferences sharedPreferences;
 
     @Inject
@@ -20,5 +24,99 @@ public class PreferencesUtil {
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
     }
 
+    /**
+     * Remove property from settings file
+     *
+     * @param settingName to remove
+     */
+    private void removeSetting(String settingName) {
+        SharedPreferences.Editor e = sharedPreferences.edit();
+        e.remove(settingName);
+        e.apply();
+    }
+
+    /**
+     * Look for the value of a key in the shared preferences file
+     *
+     * @param key String name of the property
+     *
+     * @return String value of the property. By default an empty String will be returned.
+     */
+    private String getStringProperty(String key) {
+        return sharedPreferences.getString(key, STRING_DEF_VALUE);
+    }
+
+    /**
+     * Look for the value of a key in the shared preferences file
+     *
+     * @param key          String name of the property
+     * @param defaultValue default value
+     *
+     * @return String value of the property. By default an empty String will be returned.
+     */
+    private String getStringProperty(String key, String defaultValue) {
+        return sharedPreferences.getString(key, defaultValue);
+    }
+
+    /**
+     * Save a String property in the shared preferences file
+     *
+     * @param key   String name of the property
+     * @param value String value of the property
+     */
+    private void setStringProperty(String key, String value) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(key, value);
+        editor.apply();
+    }
+
+    /**
+     * Save a int property in the shared preferences file
+     *
+     * @param key   String name of the property
+     * @param value int value of the property
+     */
+    private void setIntProperty(String key, int value) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(key, value);
+        editor.apply();
+    }
+
+    /**
+     * Look for the value of a key in the shared preferences file
+     *
+     * @param key String name of the property
+     *
+     * @return int value of the property. By default 0 will be returned.
+     */
+    private int getIntProperty(String key) {
+        return sharedPreferences.getInt(key, INT_DEF_VALUE);
+    }
+
+    /**
+     * Save a boolean property in the shared preferences file
+     *
+     * @param key   String name of the property
+     * @param value boolean value of the property
+     */
+    private void setBooleanProperty(String key, boolean value) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean(key, value);
+        editor.apply();
+    }
+
+    /**
+     * Look for the value of a key in the shared preferences file
+     *
+     * @param key          String name of the property
+     * @param defaultValue default value
+     *
+     * @return boolean value of the property. By default 0 will be returned.
+     */
+    private boolean getBooleanProperty(String key, boolean defaultValue) {
+        return sharedPreferences.getBoolean(key, defaultValue);
+    }
+
     // TODO Add all preferences needed
+
 }
