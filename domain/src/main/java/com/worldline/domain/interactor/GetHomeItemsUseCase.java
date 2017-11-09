ef35baa3e71b124fc2dd6c0e4b1ea3d9853bbb30
Miquel Pasanau <miquel.pasanaucatala@worldline.com>
2017-11-09 11:01:23 +0100

    Home Items Use Case

--- domain/src/main/java/com/worldline/domain/interactor/GetHomeItemsUseCase.java
+++ domain/src/main/java/com/worldline/domain/interactor/GetHomeItemsUseCase.java
@@ -1,6 +1,5 @@
 package com.worldline.domain.interactor;
 
-
 import com.worldline.domain.executor.PostExecutionThread;
 import com.worldline.domain.executor.ThreadExecutor;
 import com.worldline.domain.model.HomeItems;
@@ -12,24 +11,20 @@ import rx.Subscriber;
 public class GetHomeItemsUseCase extends Interactor{
 
     private final HomeRepository homeRepository;
-    private HomeItems homeItems;
 
-    protected GetHomeItemsUseCase(ThreadExecutor threadExecutor,
-            PostExecutionThread postExecutionThread, ApiCatalogueRepository apiCatalogueRepository, HomeRepository homeRepository) {
+    public GetHomeItemsUseCase(ThreadExecutor threadExecutor,
+            PostExecutionThread postExecutionThread, HomeRepository homeRepository) {
         super(threadExecutor, postExecutionThread);
-        //check repos
-        //checkRepository(homeRepository);
-        //this.apiCatalogueRepository = apiCatalogueRepository;
         this.homeRepository = homeRepository;
     }
 
     @Override
     protected Observable buildUseCaseObservable() {
-        return homeRepository.getList(homeItems);
+        return homeRepository.getList();
     }
 
-    public void execute(HomeItems homeItems, Subscriber useCaseSubscriber){
-        this.homeItems = homeItems;
+    @Override
+    public void execute(Subscriber useCaseSubscriber){
         super.execute(useCaseSubscriber);
     }
 }
