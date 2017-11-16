7f080760e934bc8f11bb8ca9de002d3c6d843e21
Miquel Pasanau <miquel.pasanaucatala@worldline.com>
2017-11-16 09:35:08 +0100

    DetailUseCase

--- domain/src/main/java/com/worldline/domain/interactor/GetDetailItemUseCase.java
+++ domain/src/main/java/com/worldline/domain/interactor/GetDetailItemUseCase.java
@@ -1,8 +1,33 @@
 package com.worldline.domain.interactor;
 
-/**
- * Created by A672272 on 14/11/2017.
- */
+import com.worldline.domain.executor.PostExecutionThread;
+import com.worldline.domain.executor.ThreadExecutor;
+import com.worldline.domain.repository.HomeRepository;
 
-public class getDetailItemUseCase {
+import rx.Observable;
+import rx.Subscriber;
+
+
+public class GetDetailItemUseCase extends Interactor {
+
+    private final HomeRepository homeRepository;
+    private String id;
+
+    public GetDetailItemUseCase(ThreadExecutor threadExecutor,
+            PostExecutionThread postExecutionThread, HomeRepository homeRepository) {
+        super(threadExecutor, postExecutionThread);
+        this.homeRepository = homeRepository;
+    }
+
+    @Override
+    protected Observable buildUseCaseObservable() {
+        return homeRepository.get(id);
+    }
+
+
+    public void execute(String id, Subscriber useCaseSubscriber){
+        this.id = id;
+        super.execute(useCaseSubscriber);
+    }
 }
+
